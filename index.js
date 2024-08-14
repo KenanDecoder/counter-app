17af6926bbabebc3babce5836f5915a9e1b2d6a9
KenanDecoder <sallamkenan752@gmail.com>
2024-08-14 06:19:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+/* 
+    1 -()- variables  
+    2 -()- if statements
+    3 -()- for loop 
+    4 -()- functions
+    5 -()- event listenter
+    6 -()- data types like (arrays , numbers , strings , objects)
+    6 -()- some motheds like (Number())
+*/
+
+
+/* 
+    this app have 
+    1 error text > 
+        ()1* if input have 0 value Error => 
+        ()2* if input have 0 value and you wanna increase or decrease value Error => 
+        ()3* if input have any data type then number Error => 
+    2 input > 
+        ()1* can accept number type => 
+        ()2* 
+    3 counter > 
+        ()1* initialize variable conter
+    4 button one named increase > 
+        ()1* take the conter variable and add or increase (input value to it)
+    5 button tow named decrease >
+        ()1* take counter variable and remove or decrease (input value of it)
+*/
